4dc1277cbf915ac712585fdc5c318e1f66b6de60
Meher Vivek <vivekdheram@gmail.com>
2020-08-26 12:47:41 +0200

    Fetching the initial posts

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,25 @@ const app = express()
 
 //Express is big chain of middleware (pipelined) we apply to incoming requests
 
-app.use((req, res, next) => { // New Middleware
-  console.log('First middleware');
-  next(); // Call next if not sending a response
-});
-
-app.use((req, res, next) => { // Middleware Sending response
-  res.send('Hello from express!')
+app.use('/api/posts',(req, res, next) => { // Middleware Sending response
+  const posts = [
+    {
+      id:'12345asd',
+      title :'First server side post',
+      content :'coming from server'
+    },
+    {
+      id: '12345asdas',
+      title: 'Second server side post',
+      content: 'coming from server!'
+    }
+  ];
+  res.status(200).json(
+    {
+      messsage:'posts successfull !',
+      posts: posts
+    }
+  );
 });
 
 module.exports = app; //  Exporting for Connecting the express to node server where requests are handled
